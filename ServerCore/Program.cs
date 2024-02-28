f6cb39e2911799c3c340070302db7f1ac8ecb15b
bokob <bmc0520@naver.com>
2024-02-28 17:14:33 +0900

    컴파일러 최적화

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,42 +4,35 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    internal class Program
+    class Program
     {
-        static void MainThread(object state)
+        volatile static bool _stop = false;
+
+        static void ThreadMain()
         {
-            for(int i=0;i<5;i++)
-                Console.WriteLine("Hello Thread!");
+            Console.WriteLine("쓰레드 시작!");
+
+            while(_stop == false)
+            {
+                // 누군가 stop 신호를 해주기를 기다린다.
+            }
+
+            Console.WriteLine("쓰레드 종료!");
         }
 
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1); // 최소
-            ThreadPool.SetMaxThreads(5, 5); // 최대
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-            for(int i=0;i<5;i++)
-            {
-                Task t = new Task(() => { while (true) { } });
-                t.Start();
-            }
+            Thread.Sleep(1000); // ms 단위, 1000 -> 1초, 메인 스레드가 1초동안 잠든다.
 
-            //for(int i=0;i<5;i++)
-            //    ThreadPool.QueueUserWorkItem((obj) => { while (true) { } });
+            _stop = true;
 
-            ThreadPool.QueueUserWorkItem(MainThread);
-
-            //Thread t = new Thread(MainThread);
-            //t.Name = "Test Thread";
-            //t.IsBackground = true;
-            //t.Start();
-            //Console.WriteLine("Waiting for Thread World!");
-
-            //t.Join();
-            //Console.WriteLine("Hello, World!");
-            while(true)
-            {
-
-            }
+            Console.WriteLine("Stop 호출");
+            Console.WriteLine("종료 대기중");
+            t.Wait();
+            Console.WriteLine("종료 성공");
         }
     }
 }
