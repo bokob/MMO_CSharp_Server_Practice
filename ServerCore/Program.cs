a1d687d668986f1f882432c9fbbd7c471b75f893
bokob <bmc0520@naver.com>
2024-03-03 16:57:52 +0900

    DeadLock

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,37 +4,63 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock(_lock)
+            {
+
+            }
+        }
+        public static void Test()
+        {
+            lock( _lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock(_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+        public static void TestUser()
+        {
+            lock(_lock)
+            {
+                
+            }
+        }
+    }
+
     class Program
     {
         static int number = 0;
         static object _obj = new object();
         static void Thread_1()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                // 상호배제 Mutual Exclusive
-                Monitor.Enter(_obj); // 잠금
-                {
-                    number++;
-                    if(number == 10000)
-                    {
-                        Monitor.Exit(_obj);
-                        return;
-                    }
-                }
-                Monitor.Exit(_obj); // 잠금 해제
+                SessionManager.Test();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                Monitor.Enter(_obj);
-                
-                number--;
-
-                Monitor.Exit(_obj);
+                UserManager.Test();
             }
         }
 
@@ -43,6 +69,9 @@ namespace ServerCore
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
+
+            Thread.Sleep(100);
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
