de82ec8d07a690c7f8896afcca2aaa017077b5bb
bokob <bmc0520@naver.com>
2024-03-06 08:54:38 +0900

    Thread Local Storage

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,44 +6,34 @@ namespace ServerCore
 {
     class Program
     {
-        // 락 구현 방법
-        // 1. 근성
-        // 2. 양보
-        // 3. 갑질
-        static object _lock = new object();
-        static SpinLock _lock2 = new SpinLock();
-
-        class Reward
-        {
-
-        }
-
-        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
-
-        // 99.999%
-        static Reward GetRewardById(int Id)
-        {
-            _lock3.EnterReadLock();
-
-            _lock3.ExitReadLock();
-
-            return null;
-        }
-
-        // 0.001%
-        void AddReward(Reward reward)
-        {
-            _lock3.EnterWriteLock();
-
-            _lock3.ExitWriteLock();
-        }
-
+        static volatile int count = 0;
+        static Lock _lock = new Lock();
         static void Main(string[] args)
         {
-            lock(_lock)
+            Task t1 = new Task(delegate ()
             {
+                for(int i=0;i<100000;i++)
+                {
+                    _lock.ReadLock();
+                    count++;
+                    _lock.ReadUnlock();
+                }
+            });
+            Task t2 = new Task(delegate ()
+            {
+                for (int i = 0; i < 100000; i++)
+                {
+                    _lock.ReadLock();
+                    count--;
+                    _lock.ReadUnlock();
+                }
+            });
 
-            }
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+            Console.WriteLine(count);
         }
     }
 }
